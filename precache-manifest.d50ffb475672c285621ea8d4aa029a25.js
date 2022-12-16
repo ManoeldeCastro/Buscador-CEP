52bbb35d9fbd44f70d7b716023b36d4ae426e07b
manoel <manoeldiasmf@outlook.com>
2022-12-16 10:40:24 -0300

    Updates

---
+++ precache-manifest.d50ffb475672c285621ea8d4aa029a25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1879a350ead72553cab9",
+    "url": "/Buscador-CEP/static/css/main.42e65364.chunk.css"
+  },
+  {
+    "revision": "1879a350ead72553cab9",
+    "url": "/Buscador-CEP/static/js/main.1879a350.chunk.js"
+  },
+  {
+    "revision": "d6b7553b929fe87f7e9f",
+    "url": "/Buscador-CEP/static/js/1.d6b7553b.chunk.js"
+  },
+  {
+    "revision": "0ee5b6be0d0e5aba3c05",
+    "url": "/Buscador-CEP/static/js/runtime~main.0ee5b6be.js"
+  },
+  {
+    "revision": "cda1aada82f1f7da3e6e60aae9ba4707",
+    "url": "/Buscador-CEP/index.html"
+  }
+];
